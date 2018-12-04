6ef9a243fad2abf668eb02d0ec5847856bf2b7ef
Zach Bradshaw <bradshaw.zachary@gmail.com>
2018-12-04 17:39:07 -0500

    feat(2018 day 3): part one solution

--- 2018/3/index.js
+++ 2018/3/index.js
@@ -4,16 +4,6 @@ const input = fs
   .readFileSync(path.resolve(__dirname, "./input.txt"), "utf8")
   .trim();
 
-const generateGrid = size => {
-  let grid = [];
-  for (let x = 0; x < size; x++) {
-    for (let y = 0; y < size; y++) {
-      grid.push({ value: null, x: x, y: y });
-    }
-  }
-  return grid;
-};
-
 const testInput = "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2";
 function partOne(input) {
   const claims = input.split("\n").map(claim => {
@@ -21,42 +11,28 @@ function partOne(input) {
 
     return {
       id,
-      topLeft: { x, y },
-      size: { width, height }
+      x,
+      y,
+      width,
+      height
     };
   });
-
-  const grid = generateGrid(8);
+  const coords = {};
   claims.forEach(claim => {
-    const { id } = claim;
-    const { x, y } = claim.topLeft;
-    const { width, height } = claim.size;
-
-    const topLeft = grid.find(inch => inch.x === x && inch.y === y);
-    topLeft.value = id;
-
+    const { id, x, y, width, height } = claim;
     for (let i = 0; i < width; i++) {
       for (let j = 0; j < height; j++) {
-        let claimedInch = grid.find(
-          inch => inch.x === x + i && inch.y === y + j
-        );
-
-        if (claimedInch.value !== null) {
-          claimedInch.value = "X";
-        } else {
-          claimedInch.value = id;
-        }
+        let currentX = x + i;
+        let currentY = y + j;
+        !coords.hasOwnProperty(`${currentX},${currentY}`)
+          ? (coords[`${currentX},${currentY}`] = { id, value: 1 })
+          : (coords[`${currentX},${currentY}`].value =
+              coords[`${currentX},${currentY}`].value + 1);
       }
     }
   });
-  let count = 0;
-  grid.forEach(inch => {
-    if (inch.value === "X") {
-      count++;
-    }
-  });
-  console.log(count);
-  // console.log(grid);
+  console.log(coords);
+  console.log(Object.values(coords).filter(count => count.value > 1).length);
 }
 
 partOne(input);
