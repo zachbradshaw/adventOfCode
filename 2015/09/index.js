36e4b7f104ad02ba825075254361bf7f6c47b4ce
Zach Bradshaw <bradshaw.zachary@gmail.com>
2019-09-05 22:57:17 -0400

    feat(2015): day 9 solution

--- 2015/09/index.js
+++ 2015/09/index.js
@@ -4,25 +4,64 @@ const input = fs
   .readFileSync(path.resolve(__dirname, "./input.txt"), "utf8")
   .trim();
 
-const locations = [];
-const distances = [];
+const locationNames = [];
+const locations = {};
+const routes = [];
 input.split("\n").forEach(line => {
-  const stripChars = line.replace(/to|=/g, " ");
-  console.log(stripChars.trim().split(" "));
-  const [start, end, distance] = stripChars.split(" ");
-
-  if (locations.indexOf(start) < 0) {
-    locations.push(start);
-
-    if (locations.indexOf(end) < 0) {
-      locations.push(end);
-    }
+  const [start, end, distance] = line.match(/([A-Z])\w+|\d+/g);
+  if (locationNames.indexOf(start) < 0) {
+    locationNames.push(start);
   }
-
-  if (distances.indexOf({ start, end, distance }) < 0) {
-    distances.push({ start, end, distance });
+  if (locationNames.indexOf(end) < 0) {
+    locationNames.push(end);
+  }
+  if (routes.indexOf({ start, end, distance }) < 0) {
+    routes.push({ start, end, distance });
   }
 });
 
-console.log(locations);
-console.log(distances.sort((a, b) => Number(a.distance) - Number(b.distance)));
+locationNames.forEach(name => (locations[name] = {}));
+routes.forEach(route => {
+  if (!locations[route.start].hasOwnProperty(route.end)) {
+    locations[route.start] = {
+      ...locations[route.start],
+      [route.end]: Number(route.distance)
+    };
+  }
+  if (!locations[route.end].hasOwnProperty(route.start)) {
+    locations[route.end] = {
+      ...locations[route.end],
+      [route.start]: Number(route.distance)
+    };
+  }
+});
+
+const getAllPossibleRoutes = options => {
+  let results = [];
+  if (options.length === 1) {
+    results.push(options);
+    return results;
+  }
+
+  for (let i = 0; i < options.length; i++) {
+    let firstOption = options[i];
+    let optionsLeft = options.slice(0, i).concat(options.slice(i + 1));
+    let innerOptions = getAllPossibleRoutes(optionsLeft);
+    for (let j = 0; j < innerOptions.length; j++) {
+      results.push([firstOption].concat(innerOptions[j]));
+    }
+  }
+  return results;
+};
+
+const distances = [];
+getAllPossibleRoutes(locationNames).forEach(route => {
+  let distance = 0;
+  route.forEach((stop, i) => {
+    if (i !== 0) {
+      distance = distance + locations[stop][route[i - 1]];
+    }
+  });
+  distances.push(distance);
+});
+console.log(distances.sort((a, b) => b - a)[0]);
