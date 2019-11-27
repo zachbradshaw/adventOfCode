b0b7781fea80f317a6be5b9c068b7b6f6206d6c4
Zach Bradshaw <bradshaw.zachary@gmail.com>
2019-11-27 14:44:55 -0500

    feat(2015): day 11 solution
    It works, but takes forever for part 2. Two stars is two stars though.

--- 2015/11/index.js
+++ 2015/11/index.js
@@ -1,62 +1,82 @@
 const input = "hxbxwxba";
 
-function checkPassword(password) {
+function passwordValidator(password) {
   let hasStraight = false;
   let pairCount = 0;
   for (let i = 0; i < password.length; i++) {
     if (i === password.length - 1) {
       break;
     }
+    // count pairs, but don't double count three of the
+    // same chars in a row
     if (password[i] === password[i + 1]) {
       if (password[i] !== password[i - 1]) {
         pairCount++;
       }
     }
-    if (password[i + 1] % password[i] === 1) {
+    // check for straight character sets like 'abc'
+    if (password[i + 1] - password[i] === 1) {
       if (i === password.length - 2) {
         break;
       }
       if (
         password[i + 2] > password[i + 1] &&
-        password[i + 2] % password[i + 1] === 1
+        password[i + 2] - password[i + 1] === 1
       ) {
         hasStraight = true;
       }
     }
   }
-  // console.log(password.map(num => validChars[num]).join(""));
-  // console.log({ hasStraight: hasStraight, pairCount: pairCount });
   return hasStraight && pairCount >= 2;
 }
 
 // prettier-ignore
 const validChars = ["a","b","c","d","e","f","g","h","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z"];
-
-function partOne(input) {
-  if (checkPassword(input)) {
-    console.log("new password:", input.map(num => validChars[num]).join(""));
-    return;
+let validPasswords = [];
+function passwordGenerator(input, passwordCount) {
+  if (passwordValidator(input)) {
+    if (validPasswords.length < passwordCount) {
+      validPasswords.push(input.map(num => validChars[num]).join(""));
+      if (validPasswords.length === passwordCount) {
+        console.log("new password(s):", validPasswords);
+        return;
+      }
+    }
   }
-  // return false;
-
-  // count++;
 
   for (let i = input.length - 1; i >= 0; i--) {
     if (input[i] < validChars.length - 1) {
       input[i]++;
-      return partOne(input);
+      return setTimeout(() => {
+        passwordGenerator(input);
+      });
     } else if (input[i] === validChars.length - 1) {
       input[i] = 0;
     }
   }
 }
 
-partOne(
-  input.split("").map(char => {
-    return validChars.indexOf(char);
-  })
-);
+function partOne(input) {
+  return passwordGenerator(input, 1);
+}
+
+function partTwo(input) {
+  return passwordGenerator(input, 2);
+}
+
+// partOne(
+//   input.split("").map(char => {
+//     return validChars.indexOf(char);
+//   })
+// );
+
+// partTwo(
+//   input.split("").map(char => {
+//     return validChars.indexOf(char);
+//   })
+// );
 
 module.exports = {
-  partOne
+  partOne,
+  partTwo
 };
