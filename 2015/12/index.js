1a0948a09be3d46cd6da62c2bc8d203bf84d2eb4
Zach Bradshaw <bradshaw.zachary@gmail.com>
2019-12-01 19:04:35 -0500

    wip 2015/12

--- 2015/12/index.js
+++ 2015/12/index.js
@@ -4,6 +4,15 @@ const input = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, "./input.json"), "utf8").trim()
 );
 
+const testCases = [
+  [1, 2, 3],
+  [1, { c: "red", b: 2 }, 3],
+  { d: "red", e: [1, 2, 3, 4], f: 5 },
+  [1, "red", 5]
+];
+
+const isObject = possibleObject => !(possibleObject instanceof Array);
+
 function partOne(input) {
   let total = 0;
   JSON.stringify(input)
@@ -12,9 +21,21 @@ function partOne(input) {
   return total;
 }
 
-function partTwo(input) {}
+function partTwo(input) {
+  let total = 0;
+  Object.keys(input).forEach(key => {
+    if (isObject(input[key])) {
+      console.log("object");
+      // setTimeout(() => {
+      //   partTwo(input[key]);
+      // });
+      console.log(Object.keys(input[key]));
+    }
+  });
+}
 
-console.log(partOne(input));
+// console.log(partOne(input));
+partTwo(input);
 
 module.exports = {
   partOne,
