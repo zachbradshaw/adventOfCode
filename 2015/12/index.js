83de857786d1b9a57ff0403fdc9e38af3b53561c
Zach Bradshaw <bradshaw.zachary@gmail.com>
2019-12-02 22:06:54 -0500

    feat(2015): day 12 solution

--- 2015/12/index.js
+++ 2015/12/index.js
@@ -4,13 +4,6 @@ const input = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, "./input.json"), "utf8").trim()
 );
 
-const testCases = [
-  [1, 2, 3],
-  [1, { c: "red", b: 2 }, 3],
-  { d: "red", e: [1, 2, 3, 4], f: 5 },
-  [1, "red", 5]
-];
-
 const isObject = possibleObject => !(possibleObject instanceof Array);
 
 function partOne(input) {
@@ -18,23 +11,30 @@ function partOne(input) {
   JSON.stringify(input)
     .match(/(-?[0-9])+/g)
     .forEach(match => (total += Number(match)));
-  return total;
+  console.log("Part one:", total);
 }
 
 function partTwo(input) {
   let total = 0;
-  Object.keys(input).forEach(key => {
-    if (isObject(input[key])) {
-      console.log("object");
-      // setTimeout(() => {
-      //   partTwo(input[key]);
-      // });
-      console.log(Object.keys(input[key]));
+  const traverse = node => {
+    if (node && typeof node === "object") {
+      Object.entries(node).forEach(([key, val]) => {
+        if (isObject(val) && Object.values(val).includes("red")) {
+          val = {};
+        }
+        traverse(val);
+      });
+    } else {
+      if (typeof node === "number") {
+        total += Number(node);
+      }
     }
-  });
+  };
+  traverse(input);
+  console.log("Part two:", total);
 }
 
-// console.log(partOne(input));
+partOne(input);
 partTwo(input);
 
 module.exports = {
