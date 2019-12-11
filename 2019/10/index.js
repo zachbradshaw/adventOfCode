0636f3ad729d11b966266f83b87ce110672dcf5b
Zach Bradshaw <bradshaw.zachary@gmail.com>
2019-12-11 11:25:50 -0500

    feat(2019): day 10.2 solution

--- 2019/10/index.js
+++ 2019/10/index.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
 const generateGrid = require("../../lib/generateGrid");
 const input = fs
   .readFileSync(path.resolve(__dirname, "./input.txt"), "utf8")
@@ -10,47 +9,107 @@ const calculateSlope = (x1, y1, x2, y2) => {
   return Math.atan2(y1 - y2, x1 - x2) * (180 / Math.PI);
 };
 
-const partOne = input => {
-  const grid = generateGrid(input.split("\n")[0].length, {
-    asteroid: false
+const grid = generateGrid(input.split("\n")[0].length, {
+  asteroid: false
+});
+input
+  .split("\n")
+  .join("")
+  .split("")
+  .forEach((point, index) => {
+    if (point === "#") {
+      grid[index] = { ...grid[index], asteroid: true };
+    }
   });
-  input
-    .split("\n")
-    .join("")
-    .split("")
-    .forEach((point, index) => {
-      if (point === "#") {
-        grid[index] = { ...grid[index], asteroid: true };
-      }
-    });
+const asteroids = grid.filter(point => point.asteroid);
 
-  for (let i = 0; i < grid.length; i++) {
-    let station = grid[i];
+const partOne = asteroids => {
+  for (let i = 0; i < asteroids.length; i++) {
     let slopes = {};
-    if (station.asteroid) {
-      for (let j = 0; j < grid.length; j++) {
-        let slope = calculateSlope(grid[i].x, grid[i].y, grid[j].x, grid[j].y);
-        if (slope === Math.PI) {
-          break;
-        }
-        if (!slopes.hasOwnProperty(slope)) {
-          slopes[slope] = grid[j].asteroid ? true : false;
-        } else {
-          if (!slope[slope] && grid[j].asteroid) {
-            slopes[slope] = true;
-          }
-        }
+
+    for (let j = 0; j < asteroids.length; j++) {
+      let slope = calculateSlope(
+        asteroids[i].x,
+        asteroids[i].y,
+        asteroids[j].x,
+        asteroids[j].y
+      );
+      if (slope === Math.PI) {
+        break;
+      }
+      if (!slopes.hasOwnProperty(slope)) {
+        slopes[slope] = 1;
       }
     }
-    grid[i] = {
-      ...grid[i],
-      // slopes
-      visibleAsteroids: Object.values(slopes).filter(slope => slope).length
+
+    asteroids[i] = {
+      ...asteroids[i],
+      visibleAsteroids: Object.values(slopes).reduce(
+        (acc, curr) => acc + curr,
+        0
+      )
     };
   }
-  // console.log(util.inspect(grid, false, null, true));
-  console.log(grid.sort((a, b) => b.visibleAsteroids - a.visibleAsteroids)[0]);
-  // console.log(grid);
+
+  const optimalStation = asteroids.sort(
+    (a, b) => b.visibleAsteroids - a.visibleAsteroids
+  )[0];
+
+  console.log("Part one:", optimalStation);
 };
 
-partOne(input);
+const partTwo = asteroids => {
+  const optimalStation = { x: 22, y: 25, slopes: {}, orderedAsteroids: [] };
+  let filteredAsteroids = asteroids.filter(a => {
+    if (a.x === optimalStation.x) {
+      if (a.y === optimalStation.y) {
+        return false;
+      }
+    }
+    return a;
+  });
+
+  for (let i = 0; i < filteredAsteroids.length; i++) {
+    let slope = calculateSlope(
+      optimalStation.x,
+      optimalStation.y,
+      filteredAsteroids[i].x,
+      filteredAsteroids[i].y
+    );
+    if (slope === Math.PI) {
+      break;
+    }
+    if (!optimalStation.slopes.hasOwnProperty(slope)) {
+      optimalStation.slopes[slope] = [filteredAsteroids[i]];
+    } else {
+      optimalStation.slopes[slope].push(filteredAsteroids[i]);
+    }
+  }
+
+  let sortedSlopes = Object.keys(optimalStation.slopes).sort((a, b) => a - b);
+  let startingSlope = sortedSlopes.indexOf("90");
+  sortedSlopes.push(...sortedSlopes.splice(0, startingSlope));
+
+  sortedSlopes.forEach(slope => {
+    optimalStation.orderedAsteroids.push(
+      optimalStation.slopes[slope].reverse()
+    );
+  });
+
+  let record = {};
+  let counter = 1;
+  for (i = 0; i < optimalStation.orderedAsteroids.length; i++) {
+    if (!optimalStation.orderedAsteroids[i]) {
+      i = -1;
+    }
+    if (optimalStation.orderedAsteroids[i].length === 0) {
+      break;
+    }
+    record[counter] = optimalStation.orderedAsteroids[i].shift();
+    counter++;
+  }
+  console.log("Part two", record[200]);
+};
+
+partOne(asteroids);
+partTwo(asteroids);
