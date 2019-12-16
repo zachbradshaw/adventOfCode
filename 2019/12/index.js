82980affc0cda8e0cafd649aa938640847da5e5b
Zach Bradshaw <bradshaw.zachary@gmail.com>
2019-12-16 16:37:37 -0500

    wip 2019 day 12.2

--- 2019/12/index.js
+++ 2019/12/index.js
@@ -1,9 +1,10 @@
-const moons = [
-  { position: { x: 1, y: 2, z: -9 }, velocity: { x: 0, y: 0, z: 0 } },
-  { position: { x: -1, y: -9, z: -4 }, velocity: { x: 0, y: 0, z: 0 } },
-  { position: { x: 17, y: 6, z: 8 }, velocity: { x: 0, y: 0, z: 0 } },
-  { position: { x: 12, y: 4, z: 2 }, velocity: { x: 0, y: 0, z: 0 } }
-];
+const path = require("path");
+const fs = require("fs");
+const input = fs
+  .readFileSync(path.resolve(__dirname, "./input.txt"), "utf8")
+  .trim();
+
+const parseInput = str => str.match(/-?\d+/g);
 
 const XYZ = ["x", "y", "z"];
 const applyGravity = moons =>
@@ -36,11 +37,52 @@ const loop = moons => {
 const getEnergy = args =>
   Object.values(args).reduce((acc, curr) => Math.abs(acc) + Math.abs(curr), 0);
 
-const totalEnergy = moons
-  .map(moon => {
-    return {
-      ...moon,
-      totalEnergy: getEnergy(moon.position) * getEnergy(moon.velocity)
-    };
-  })
-  .reduce((acc, curr) => acc + curr.totalEnergy, 0);
+const partOne = input => {
+  const moons = [];
+  input.split("\n").forEach(line => {
+    const [x, y, z] = parseInput(line).map(Number);
+    moons.push({ position: { x, y, z }, velocity: { x: 0, y: 0, z: 0 } });
+  });
+
+  for (let i = 0; i < 10; i++) {
+    loop(moons);
+  }
+
+  const totalEnergy = moons
+    .map(moon => {
+      return {
+        ...moon,
+        totalEnergy: getEnergy(moon.position) * getEnergy(moon.velocity)
+      };
+    })
+    .reduce((acc, curr) => acc + curr.totalEnergy, 0);
+
+  console.log("Part one:", totalEnergy);
+};
+
+const partTwo = input => {
+  const moons = [];
+  const original = [];
+  input.split("\n").forEach(line => {
+    const [x, y, z] = parseInput(line).map(Number);
+    moons.push({ position: { x, y, z }, velocity: { x: 0, y: 0, z: 0 } });
+    original.push({ position: { x, y, z }, velocity: { x: 0, y: 0, z: 0 } });
+  });
+
+  for (let i = 0; i <= 2772; i++) {
+    if (
+      i !== 0 &&
+      JSON.stringify(original[0].position.x) ===
+        JSON.stringify(moons[0].position.x)
+    ) {
+      console.log(moons);
+      console.log("Iteration:", i);
+      console.log("*****************");
+    }
+
+    loop(moons);
+  }
+};
+
+// partOne(input);
+partTwo(input);
