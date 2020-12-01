69af0558e55cea00a808287ae743315dc745958a
Zach Bradshaw <zbradshaw@statestitle.com>
2020-12-01 08:57:42 -0500

    2020 day 1 cleanup

--- src/2020/01.ts
+++ src/2020/01.ts
@@ -4,29 +4,35 @@ import fs from 'fs'
 const input = fs
   .readFileSync(path.resolve(__dirname, process.argv[2]), 'utf8')
   .trim()
+  .split('\n')
+  .map(Number)
 
-const partOne = () => {
-  const splitInput = input.split('\n').map(Number)
-  for (let i = 0; i < splitInput.length; i++) {
-    for (let j = 0; j < splitInput.length; j++) {
-      if (splitInput[i] + splitInput[j] === 2020) {
-        return splitInput[i] * splitInput[j]
+const partOne = (): number => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+      if (input[i] + input[j] === 2020) {
+        return input[i] * input[j]
       }
     }
   }
+  return 0
 }
 
-const partTwo = () => {
-  const splitInput = input.split('\n').map(Number)
-  for (let i = 0; i < splitInput.length; i++) {
-    for (let j = 0; j < splitInput.length; j++) {
-      for (let k = 0; k < splitInput.length; k++) {
-        if (splitInput[i] + splitInput[j] + splitInput[k] === 2020) {
-          return splitInput[i] * splitInput[j] * splitInput[k]
+const partTwo = (): number => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+      for (let k = 0; k < input.length; k++) {
+        if (input[i] + input[j] + input[k] === 2020) {
+          return input[i] * input[j] * input[k]
         }
       }
     }
   }
+  return 0
 }
-console.log(partOne())
-console.log(partTwo())
+
+console.log('--- Day 1: Report Repair ---')
+console.time('Completed in')
+console.log('Part one:', partOne())
+console.log('Part two:', partTwo())
+console.timeEnd('Completed in')
